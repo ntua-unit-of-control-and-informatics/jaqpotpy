a4dcfb6c54d4512be9cf000f60574f4b0ff21671
alex.arvanitidis <alex.arvanitidis@mimacom.com>
2024-04-29 12:55:56 +0300

    fix: run model properly

--- run_locally.py
+++ run_locally.py
@@ -7,17 +7,19 @@ from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+# Train model
 smiles = ['CCCCCCC', 'C1CCC1', 'CC(=O)CC', 'CCCCCCCC', 'CCCC1CC1']
-y = [6.5, 3.4, 2.4, 1.5, 7. ]
+y = [6.5, 3.4, 2.4, 1.5, 7]
 
 featurizer = MACCSKeysFingerprint()
-dataset = SmilesDataset(smiles=smiles, y=y, featurizer=featurizer, task='regression')
-dataset.create()
+training_dataset = SmilesDataset(smiles=smiles, y=y, featurizer=featurizer, task='regression')
+training_dataset.create()
 lr = LinearRegression()
 
-model = MolecularSKLearn(dataset, doa=Leverage(), model=lr, eval=None)
+model = MolecularSKLearn(training_dataset, doa=Leverage(), model=lr, eval=None)
 fitted = model.fit()
 
+# Upload to local jaqpot
 # api_key = ''  # set your api key here
 #
 # jaqpot = Jaqpot(baseUrl="http://localhost:8080/jaqpot/services/")
@@ -26,4 +28,7 @@ fitted = model.fit()
 #                         description="ALEX",
 #                         model_title="RANDOM")
 
+# Infer
+input_dataset = ['CC']
+fitted(input_dataset)
 print(fitted.prediction)
