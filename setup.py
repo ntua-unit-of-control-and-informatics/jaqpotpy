edc48ad7b003b0d14eb99bde7e926686917c845b
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-05-28 13:08:02 +0300

    fix: Update the versions of dependencies.
    This commit sets all dependecies of jaqpotpy to updated and fixed versions, both in setup.py and requirements.txt files.

--- setup.py
+++ setup.py
@@ -33,9 +33,34 @@ setup(name='jaqpotpy',
       #           , 'jaqpotpy.datasets', 'jaqpotpy.descriptors', 'jaqpotpy.descriptors.molecular'
       #           , 'jaqpotpy.descriptors.graph'],
       install_requires=[
-            'pandas', 'requests', 'pydantic', 'rdkit-pypi', 'mordred', 'pyjwt', 'scikit-learn', "tqdm"
-            , 'skl2onnx', 'onnxruntime'
-            # ,'tornado', 'scikit-learn', 'torch', 'torch-scatter', 'torch-sparse', 'torch-cluster'
-            # , 'torch-spline-conv', 'torch-geometric', 'dgl', 'torch_sparse', 'kennard-stone'
+      #     'pandas', 'requests', 'pydantic', 'rdkit-pypi', 'mordred', 'pyjwt', 'scikit-learn', 
+      #     'tqdm', 'skl2onnx', 'onnxruntime'
+           
+            'pandas==2.2.2',
+            'pyjwt==2.8.0',
+            'simplejson==3.19.2',
+            'pydotplus==2.0.2',
+            'requests==2.32.2',
+            'pydantic==2.7.1',
+            'rdkit-pypi==2022.9.5',
+            'mordred==1.2.0',
+            'scikit-learn==1.5.0',
+            'tqdm==4.66.4',
+            'kennard-stone==2.2.1',
+            'mendeleev==0.16.2',
+            'pymatgen==2024.5.1',
+            'skl2onnx==1.16.0',
+            'onnxruntime==1.18.0',
+
+            # # Torch requirements
+            'torch==2.3.0',
+            'torch-geometric==2.3.1',
+            'torchvision==0.18.0',
+            # 'torch-scatter==2.1.2',
+            # 'torch-sparse==0.6.18',
+            # 'torch-cluster==1.6.3',
+            # 'torch-spline-conv==1.2.2'
+            # # ,'tornado', 'scikit-learn', 'torch', 'torch-scatter', 'torch-sparse', 'torch-cluster'
+            # # , 'torch-spline-conv', 'torch-geometric', 'dgl', 'torch_sparse', 'kennard-stone'
       ],
       zip_safe=False)
