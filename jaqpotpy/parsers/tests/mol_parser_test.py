6f3680c0c739270a7b48b4e27612ce15bde61452
Periklis_ts <ptsirostsib@gmail.com>
2024-06-06 16:02:17 +0300

    fix: Corrected some lint errors in parser tests

--- jaqpotpy/parsers/tests/mol_parser_test.py
+++ jaqpotpy/parsers/tests/mol_parser_test.py
@@ -2,6 +2,7 @@
 Tests for Jaqpotpy Models.
 """
 import unittest
+from jaqpotpy.parsers.mol_parser import MolParser
 
 
 
@@ -14,16 +15,15 @@ class TestParsers(unittest.TestCase):
         """
         Set up tests.
         """
-        from jaqpotpy.parsers.mol_parser.mol_parser import MolParser
 
-        mol = 'C:/Users/jason/Downloads/ChEBI_16716.mol'
-        sdf = 'C:/Users/jason/Downloads/ChEBI_16716.sdf'
-        sdfs = 'C:/Users/jason/Downloads/ChEBI_16732.sdf'
-        folder = 'C:/Users/jason/Downloads/'
+        #mol = 'C:/Users/jason/Downloads/ChEBI_16716.mol'
+        #sdf = 'C:/Users/jason/Downloads/ChEBI_16716.sdf'
+        #sdfs = 'C:/Users/jason/Downloads/ChEBI_16732.sdf'
+        #folder = 'C:/Users/jason/Downloads/'
 
-        self.parser = MolParser(sdfs, 'sdf')
+        #self.parser = MolParser(sdfs, 'sdf')
 
-    @unittest.skip("This test needs refactoring)  
+    @unittest.skip("This test needs refactoring") 
     def test_mol(self):
         """
         Test mol file.
@@ -33,7 +33,7 @@ class TestParsers(unittest.TestCase):
         # print(mol)
         return
 
-    @unittest.skip("This test needs refactoring)  
+    @unittest.skip("This test needs refactoring")  
     def test_sdf(self):
         """
         Test sdf file.
@@ -44,7 +44,7 @@ class TestParsers(unittest.TestCase):
         # # assert pdb.atoms.elements
         return
 
-    @unittest.skip("This test needs refactoring)  
+    @unittest.skip("This test needs refactoring")  
     def test_sdfs(self):
         """
         Test sdf file.
@@ -59,7 +59,7 @@ class TestParsers(unittest.TestCase):
         # assert pdb.atoms.elements
         return
 
-    @unittest.skip("This test needs refactoring)  
+    @unittest.skip("This test needs refactoring")  
     def test_path(self):
         """
         Test pdb folder.
@@ -74,10 +74,10 @@ class TestParsers(unittest.TestCase):
         #     stop = input('q to stop')
         return
         
-    @unittest.skip("This test needs refactoring)  
+    @unittest.skip("This test needs refactoring")  
     def test_df_file(self):
-        df = self.parser.parse_dataframe()
-        print(df[0],'\n\n', df[1])
+        #df = self.parser.parse_dataframe()
+        #print(df[0],'\n\n', df[1])
         return
 
 
