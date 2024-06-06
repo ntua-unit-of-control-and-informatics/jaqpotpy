845102269797fc72e50b61ae0274795eb5bf2143
Periklis_ts <ptsirostsib@gmail.com>
2024-06-06 16:42:30 +0300

    fix: Removed some secrets

--- jaqpotpy/jaqpotpy_tests/jaqpotpy_test.py
+++ jaqpotpy/jaqpotpy_tests/jaqpotpy_test.py
@@ -9,23 +9,23 @@ class TestModels(unittest.TestCase):
     @unittest.skip("Tests need refactoring")
     def test_login(self):
         jaqpot = Jaqpot()
-        jaqpot.login("pantelispanka", "")
+        jaqpot.login("", "")
 
     @unittest.skip("Tests need refactoring")
     def test_fail_login(self):
         jaqpot = Jaqpot()
-        jaqpot.login("pante", "asdf")
+        jaqpot.login("", "")
 
     @unittest.skip("Tests need refactoring")
     def test_get_model(self):
         jaqpot = Jaqpot()
-        jaqpot.login("pantelispanka", "")
+        jaqpot.login("", "")
         model = jaqpot.get_model_by_id("Yon4zUuavqY46Umu9AKp")
 
     @unittest.skip("Tests need refactoring")
     def test_load_mol_mod(self):
         jaqpot = Jaqpot()
-        jaqpot.set_api_key("..TBKGvyRK8VbwCKYEd06tbpV-Z3VhiTTBwsKOkcB9nB-ZFM31J6nEYALqi-PYO7rPX4bHeRiC83dbMGs9_OdtsiApz_ayi8QLA006CvRkuaag4SXNQqiFQ")
+        jaqpot.set_api_key("")
         model = MolecularModel().load_from_jaqpot(jaqpot, "id")
 
     @unittest.skip("Tests need refactoring")
@@ -33,7 +33,7 @@ class TestModels(unittest.TestCase):
         data = {'LFG': [0.1, 0.2], 'EQP': [0.1, 0.2], 'NEQ': [0.1, 0.2], 'GAP': [0.1, 0.2]}
         df = pd.DataFrame.from_dict(data)
         jaqpot = Jaqpot()
-        jaqpot.login("pantelispanka", "")
+        jaqpot.login("", "")
         df, predicts = jaqpot.predict(df, "Yon4zUuavqY46Umu9AKp")
         print(df)
         print(predicts)
