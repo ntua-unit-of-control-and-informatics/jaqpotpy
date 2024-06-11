07577139cf3704b329a6af6479998dd3b9b56180
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-06-11 15:37:47 +0300

    fix: print descriptors shape

--- jaqpotpy/doa/tests/test_doa.py
+++ jaqpotpy/doa/tests/test_doa.py
@@ -42,8 +42,7 @@ class TestDoa(unittest.TestCase):
 
         doa = Leverage()
         doa.fit(descriptors)
-        #print(doa._data.shape)
-        warnings.warn(f"input_shape = {doa._data.shape}")
+        warnings.warn(f"descriptors_shape = {descriptors.shape}")
         mol = [
             'C[C@@](C)(O1)C[C@@H](O)[C@@]1(O2)[C@@H](C)[C@@H]3CC=C4[C@]3(C2)C(=O)C[C@H]5[C@H]4CC[C@@H](C6)[C@]5(C)Cc(n7)c6nc(C[C@@]89(C))c7C[C@@H]8CC[C@@H]%10[C@@H]9C[C@@H](O)[C@@]%11(C)C%10=C[C@H](O%12)[C@]%11(O)[C@H](C)[C@]%12(O%13)[C@H](O)C[C@@]%13(C)CO',
             'COc1ccc2c(N)nn(C(=O)Cc3cccc(Cl)c3)c2c1', 'CN(C)c1ccc(N(Cc2ccsc2)C(=O)Cc2cncc3ccccc23)cc1']
