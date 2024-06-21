8c5333f032e3a680d19903d2c7295fa9b707e596
Giannis Pitoskas <jpitoskas@gmail.com>
2024-06-21 20:03:09 +0300

    fix: Two minor fixes in metrics
    In this commit we:
    - Add zero_division=0 to f1
    - Add labels to confusion matrix function

--- jaqpotpy/jaqpotpy_torch/trainers/binary_trainers/binary_model_trainer.py
+++ jaqpotpy/jaqpotpy_torch/trainers/binary_trainers/binary_model_trainer.py
@@ -155,7 +155,7 @@ class BinaryModelTrainer(TorchModelTrainer):
         metrics_dict = self._compute_metrics(all_labels, all_preds)
         metrics_dict['roc_auc'] = metrics.roc_auc_score(all_labels, all_probs)
         metrics_dict['loss'] = avg_loss
-        conf_mat = metrics.confusion_matrix(all_labels, all_preds)
+        conf_mat = metrics.confusion_matrix(all_labels, all_preds, labels=torch.arange(self.model.output_dim))
 
         #     tn, fp, fn, tp = metrics.confusion_matrix(all_labels, all_preds).ravel()
         #     conf_mat = {'tn': tn, 'fp': fp, 'fn': fn, 'tp': tp}
@@ -230,7 +230,7 @@ class BinaryModelTrainer(TorchModelTrainer):
         balanced_accuracy = metrics.balanced_accuracy_score(y_true, y_pred)
         precision = metrics.precision_score(y_true, y_pred, zero_division=0)
         recall = metrics.recall_score(y_true, y_pred, zero_division=0)
-        f1 = metrics.f1_score(y_true, y_pred)
+        f1 = metrics.f1_score(y_true, y_pred, zero_division=0)
         mcc = metrics.matthews_corrcoef(y_true, y_pred)
 
         metrics_dict = {
