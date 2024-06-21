08ff203cf8dfe188d34bd65d0833cdfd9292fabd
Giannis Pitoskas <jpitoskas@gmail.com>
2024-06-21 17:45:18 +0300

    fix: Confusion Matrix return as matrix not vector

--- jaqpotpy/jaqpotpy_torch/trainers/binary_trainers/binary_model_trainer.py
+++ jaqpotpy/jaqpotpy_torch/trainers/binary_trainers/binary_model_trainer.py
@@ -112,8 +112,8 @@ class BinaryModelTrainer(TorchModelTrainer):
         Returns:
             float: Average loss over the validation dataset.
             dict: Dictionary containing evaluation metrics. The keys represent the metric names and the values are floats.
-            numpy.ndarray: Confusion matrix as a numpy array of shape (4,) representing true negative (TN), false positive (FP), 
-                           false negative (FN), and true positive (TP) counts respectively. The elements are arranged as [TN, FP, FN, TP].
+            numpy.ndarray: Confusion matrix as a numpy array of shape (2, 2) representing true negative (TN), false positive (FP), 
+                           false negative (FN), and true positive (TP) counts respectively. The elements are arranged as [[TN, FP], [FN, TP]].
         """
     
         running_loss = 0
@@ -155,7 +155,7 @@ class BinaryModelTrainer(TorchModelTrainer):
         metrics_dict = self._compute_metrics(all_labels, all_preds)
         metrics_dict['roc_auc'] = metrics.roc_auc_score(all_labels, all_probs)
         metrics_dict['loss'] = avg_loss
-        conf_mat = metrics.confusion_matrix(all_labels, all_preds).ravel()
+        conf_mat = metrics.confusion_matrix(all_labels, all_preds)
 
         #     tn, fp, fn, tp = metrics.confusion_matrix(all_labels, all_preds).ravel()
         #     conf_mat = {'tn': tn, 'fp': fp, 'fn': fn, 'tp': tp}
--- jaqpotpy/jaqpotpy_torch/trainers/multiclass_trainers/multiclass_model_trainer.py
+++ jaqpotpy/jaqpotpy_torch/trainers/multiclass_trainers/multiclass_model_trainer.py
@@ -108,8 +108,8 @@ class MulticlassModelTrainer(TorchModelTrainer):
         Returns:
             float: Average loss over the validation dataset.
             dict: Dictionary containing evaluation metrics. The keys represent the metric names and the values are floats.
-            numpy.ndarray: Confusion matrix as a numpy array of shape (4,) representing true negative (TN), false positive (FP), 
-                           false negative (FN), and true positive (TP) counts respectively. The elements are arranged as [TN, FP, FN, TP].
+            numpy.ndarray: Confusion matrix C as a numpy array of shape (num_classes, num_classes), with Cij being equal to 
+                           the number of observations known to be in group i and predicted to be in group j.
         """
     
         running_loss = 0
@@ -151,7 +151,7 @@ class MulticlassModelTrainer(TorchModelTrainer):
         metrics_dict = self._compute_metrics(all_labels, all_preds)
         # metrics_dict['roc_auc'] = metrics.roc_auc_score(all_labels, all_probs)
         metrics_dict['loss'] = avg_loss
-        conf_mat = metrics.confusion_matrix(all_labels, all_preds).ravel()
+        conf_mat = metrics.confusion_matrix(all_labels, all_preds)
 
         #     tn, fp, fn, tp = metrics.confusion_matrix(all_labels, all_preds).ravel()
         #     conf_mat = {'tn': tn, 'fp': fp, 'fn': fn, 'tp': tp}
