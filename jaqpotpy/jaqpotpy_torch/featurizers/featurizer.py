d2431fd5611a03a293d1e2940386cd52b066a9b4
Giannis Pitoskas <jpitoskas@gmail.com>
2024-06-26 13:15:57 +0300

    refactor: SmilesGraphFeaturizer code in docs
    This commit:
    - Adds example code blocks in SmilesGraphFeaturizer
    - Sets '.' instead of '_' for the separator when showing atom/bond characteristics vector labels
    - Add __call__ to to call abstract featurize() method in abstract class Featurizer

--- jaqpotpy/jaqpotpy_torch/featurizers/featurizer.py
+++ jaqpotpy/jaqpotpy_torch/featurizers/featurizer.py
@@ -6,6 +6,27 @@ Contact: jpitoskas@gmail.com
 from abc import ABC, abstractmethod
 
 class Featurizer(ABC):
+    """
+    Abstract base class for featurizers.
+    """
+
+    def __call__(self, *args, **kwargs):
+        """
+        Featurizes the input data.
+        
+        Returns:
+            The featurized data.
+        """
+        return self.featurize(*args, **kwargs)
+    
     @abstractmethod
     def featurize(self, *args, **kwargs):
+        """
+        Abstract method to featurize the input data.
+
+        Returns:
+            The featurized data.
+        """
         pass
+
+    
