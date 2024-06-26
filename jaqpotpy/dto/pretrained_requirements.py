3bfe224f457f78f2d4336b9b9324de36d23a0dfc
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-06-26 16:28:23 +0300

    feat: Created `PretrainedRequirements_v2`, compatible with the new API

--- jaqpotpy/dto/pretrained_requirements.py
+++ jaqpotpy/dto/pretrained_requirements.py
@@ -40,3 +40,29 @@ class PretrainedRequirements:
         #     self.description = description
         # if dependentFeatures is not None:
         #     self.dependentFeatures = dependentFeatures
+
+class PretrainedRequirements_v2:
+
+    def __init__(self, id=None, meta=None, name=None, description=None, type=None,
+                 jaqpotpyVersion=None, libraries=None, dependentFeatures=None,
+                 independentFeatures=None, organizations=None, visibility=None,
+                 reliability=None, pretrained=None, actualModel=None, creator=None,
+                 canEdit=None, createdAt=None, updatedAt=None):
+        self.id = id
+        self.meta = meta
+        self.name = name
+        self.description = description
+        self.type = type
+        self.jaqpotpyVersion = jaqpotpyVersion
+        self.libraries = libraries
+        self.dependentFeatures = dependentFeatures
+        self.independentFeatures = independentFeatures
+        self.organizations = organizations
+        self.visibility = visibility
+        self.reliability = reliability
+        self.pretrained = pretrained
+        self.actualModel = actualModel
+        self.creator = creator
+        self.canEdit = canEdit
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
