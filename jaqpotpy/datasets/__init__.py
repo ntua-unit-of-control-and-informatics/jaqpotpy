4358c263df3dc304dac5ab459110aa44920db3fe
IOANNIS SAVVAS <149728671+johnsaveus@users.noreply.github.com>
2024-07-18 12:18:02 +0300

    feat(JAQPOT-198): create graph dataset (#38)
    * Graph_featurizer
    
    graph_featurizer script and its test
    
    * New_Graph_featurizer_tests
    
    new tests and some original code fix
    
    * Dataset_finished_with_tests

--- jaqpotpy/datasets/__init__.py
+++ jaqpotpy/datasets/__init__.py
@@ -3,3 +3,4 @@
 from .image_datasets import TorchImageDataset
 from .molecular_datasets import JaqpotpyDataset, TorchGraphDataset
 from .material_datasets import CompositionDataset, StructureDataset
+from .graph_pyg_dataset import SmilesGraphDataset
