ba78d274a487a3636e3c34ee5f35357f33043115
johnsaveus <ioannis.savvas00@gmail.com>
2024-07-25 14:26:53 +0300

    fix: remove_old_inits

--- jaqpotpy/models/__init__.py
+++ jaqpotpy/models/__init__.py
@@ -2,11 +2,11 @@ from jaqpotpy.models.evaluator import Evaluator
 from jaqpotpy.models.preprocessing import Preprocess
 from jaqpotpy.models.base_classes import MolecularModel, Model, MaterialModel
 from jaqpotpy.models.sklearn import SklearnModel
-try:
-    from jaqpotpy.models.torch_geometric import MolecularTorchGeometric
-    from jaqpotpy.models.torch import MolecularTorch
-    from jaqpotpy.models.torch_models.torch_geometric import *
-    from jaqpotpy.models.torch import MolecularTorch, MaterialTorch
-except ModuleNotFoundError:
-    MolecularTorchGeometric = MolecularTorch = GCN = MolecularTorch = MaterialTorch = None
-    pass
+#try:
+    #from jaqpotpy.models.torch_geometric import MolecularTorchGeometric
+    #from jaqpotpy.models.torch import MolecularTorch
+    #from jaqpotpy.models.torch_models.torch_geometric import *
+    #from jaqpotpy.models.torch import MolecularTorch, MaterialTorch
+#except ModuleNotFoundError:
+#    MolecularTorchGeometric = MolecularTorch = GCN = MolecularTorch = MaterialTorch = None
+#    pass
