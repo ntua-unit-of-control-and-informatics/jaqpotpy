991c9f22ad899ee72ab74d5fba33cc6075686e88
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-07-25 15:38:49 +0300

    chore: remove old code from datasets

--- jaqpotpy/datasets/__init__.py
+++ jaqpotpy/datasets/__init__.py
@@ -1,6 +1,5 @@
 #from jaqpotpy.datasets import *
 #from jaqpotpy.datasets.molecular_datasets import *
 from .image_datasets import TorchImageDataset
-from .molecular_datasets import JaqpotpyDataset, TorchGraphDataset
-from .material_datasets import CompositionDataset, StructureDataset
+from .molecular_datasets import JaqpotpyDataset
 from .graph_pyg_dataset import SmilesGraphDataset
