86ac526b759943af9c7f68512ebf0bec46f66393
johnsaveus <ioannis.savvas00@gmail.com>
2024-09-18 21:36:56 +0300

    chore
    delete jaqpotpy_schemas. No reason to exist. We take them from the api

--- jaqpotpy/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Author: Ioannis Pitoskas (jpitoskas@gmail.com)"""
-
-from .feature import Feature
-from .library import Library
-from .organization import Organization
-
-_all_ = ["Feature", "Library", "Organization"]
