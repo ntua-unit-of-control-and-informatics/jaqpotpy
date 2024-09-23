d3ccb1da7cbfefb6a7954e7eeddb1c0bbb8241d0
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-09-23 16:56:44 +0300

    chore: bring back cfg

---
+++ jaqpotpy/cfg/config.py
@@ -0,0 +1,17 @@
+import os
+
+"""
+Configuration variables for Jaqpotpy
+"""
+
+"""
+x -> Test variable
+verbode -> Verbose outputs. eg descriptor creation, model training
+global_seed -> Seed for various descriptors and models
+version -> Jaqpotpy version
+"""
+
+x = 0
+verbose = True
+global_seed = 42
+# version = jaqpotpy.__version__
---
+++ jaqpotpy/cfg/tests/test_config.py
@@ -0,0 +1,8 @@
+import unittest
+from jaqpotpy.cfg import config
+
+
+@unittest.skip("Not clear what this test was supposed to do")
+class TestConfig(unittest.TestCase):
+    def test_config_1(self):
+        assert config.x == 0
