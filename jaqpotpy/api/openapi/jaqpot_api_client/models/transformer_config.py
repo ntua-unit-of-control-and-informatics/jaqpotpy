10fb25a126057f8e6f6c89a8641bc51f71019acf
Vassilis Minadakis <56068291+vassilismin@users.noreply.github.com>
2024-09-24 09:54:58 +0300

    refactor: Update openapi files (#64)
    * update:new openapi files
    
    * chore: update example
    
    * fix: replace with transformers

--- jaqpotpy/api/openapi/jaqpot_api_client/models/transformer_config.py
+++ jaqpotpy/api/openapi/jaqpot_api_client/models/transformer_config.py
@@ -4,17 +4,17 @@ from attrs import define as _attrs_define
 from attrs import field as _attrs_field
 
 if TYPE_CHECKING:
-    from ..models.featurizer_config_additional_property import FeaturizerConfigAdditionalProperty
+    from ..models.transformer_config_additional_property import TransformerConfigAdditionalProperty
 
 
-T = TypeVar("T", bound="FeaturizerConfig")
+T = TypeVar("T", bound="TransformerConfig")
 
 
 @_attrs_define
-class FeaturizerConfig:
+class TransformerConfig:
     """ """
 
-    additional_properties: Dict[str, "FeaturizerConfigAdditionalProperty"] = _attrs_field(init=False, factory=dict)
+    additional_properties: Dict[str, "TransformerConfigAdditionalProperty"] = _attrs_field(init=False, factory=dict)
 
     def to_dict(self) -> Dict[str, Any]:
         field_dict: Dict[str, Any] = {}
@@ -25,28 +25,28 @@ class FeaturizerConfig:
 
     @classmethod
     def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
-        from ..models.featurizer_config_additional_property import FeaturizerConfigAdditionalProperty
+        from ..models.transformer_config_additional_property import TransformerConfigAdditionalProperty
 
         d = src_dict.copy()
-        featurizer_config = cls()
+        transformer_config = cls()
 
         additional_properties = {}
         for prop_name, prop_dict in d.items():
-            additional_property = FeaturizerConfigAdditionalProperty.from_dict(prop_dict)
+            additional_property = TransformerConfigAdditionalProperty.from_dict(prop_dict)
 
             additional_properties[prop_name] = additional_property
 
-        featurizer_config.additional_properties = additional_properties
-        return featurizer_config
+        transformer_config.additional_properties = additional_properties
+        return transformer_config
 
     @property
     def additional_keys(self) -> List[str]:
         return list(self.additional_properties.keys())
 
-    def __getitem__(self, key: str) -> "FeaturizerConfigAdditionalProperty":
+    def __getitem__(self, key: str) -> "TransformerConfigAdditionalProperty":
         return self.additional_properties[key]
 
-    def __setitem__(self, key: str, value: "FeaturizerConfigAdditionalProperty") -> None:
+    def __setitem__(self, key: str, value: "TransformerConfigAdditionalProperty") -> None:
         self.additional_properties[key] = value
 
     def __delitem__(self, key: str) -> None:
--- onnx_example.py
+++ onnx_example.py
@@ -2,20 +2,20 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
-from jaqpotpy.descriptors.molecular import TopologicalFingerprint
+from jaqpotpy.descriptors.molecular import TopologicalFingerprint, MordredDescriptors
 from jaqpotpy.datasets import JaqpotpyDataset
 from jaqpotpy.models import SklearnModel
 from jaqpotpy.doa.doa import Leverage
 from jaqpotpy.models.preprocessing import Preprocess
 from jaqpotpy import Jaqpot
 
-path = "./jaqpotpy/test_data/test_data_smiles_regression_multioutput.csv"
+path = "./jaqpotpy/test_data/test_data_smiles_regression.csv"
 
-df = pd.read_csv(path)
+df = pd.read_csv(path).iloc[0:3, :]
 smiles_cols = ["SMILES"]
 y_cols = ["ACTIVITY"]
 x_cols = ["X1", "X2"]
-featurizer = TopologicalFingerprint()
+featurizer = MordredDescriptors()
 dataset = JaqpotpyDataset(
     df=df,
     y_cols=y_cols,
@@ -31,66 +31,70 @@ pre = Preprocess()
 model = RandomForestRegressor(random_state=42)
 doa_method = Leverage()
 molecularModel_t1 = SklearnModel(
-    dataset=dataset, doa=None, model=model, evaluator=None, preprocessor=pre
+    dataset=dataset, doa=None, model=model, evaluator=None, preprocessor=None
 )
 
 molecularModel_t1.fit()
-# # print(molecularModel_t1.transformers_y)
-pred_path = "./jaqpotpy/test_data/test_data_smiles_prediction_dataset.csv"
-df = pd.read_csv(pred_path)
+# # # print(molecularModel_t1.transformers_y)
+# pred_path = "./jaqpotpy/test_data/test_data_smiles_prediction_dataset.csv"
+# df = pd.read_csv(pred_path)
 
-# df = pd.DataFrame({
-#     'SMILES': ['CC'],
-#     'X1': [1],
-#     'X2': [2]
-# })
-prediction_dataset = JaqpotpyDataset(
-    df=df,
-    y_cols=None,
-    smiles_cols=smiles_cols,
-    x_cols=x_cols,
-    task="regression",
-    featurizer=featurizer,
+# # df = pd.DataFrame({
+# #     'SMILES': ['CC'],
+# #     'X1': [1],
+# #     'X2': [2]
+# # })
+# prediction_dataset = JaqpotpyDataset(
+#     df=df,
+#     y_cols=None,
+#     smiles_cols=smiles_cols,
+#     x_cols=x_cols,
+#     task="regression",
+#     featurizer=featurizer,
+# )
+# # print(prediction_dataset.df)
+
+# skl_predictions = molecularModel_t1.predict(prediction_dataset)
+# # skl_probabilities = molecularModel_t1.predict_proba(prediction_dataset)
+# onnx_predictions = molecularModel_t1.predict_onnx(prediction_dataset)
+# # onnx_probs = molecularModel_t1.predict_proba_onnx(prediction_dataset)
+# print("SKLearn Predictions:", skl_predictions)
+# # print('SKLearn Probabilities:', skl_probabilities)
+# print("ONNX Predictions:", onnx_predictions)
+# # print('ONNX Probabilities:', onnx_probs)
+
+
+# # Merge predictions and probabilities into a pandas DataFrame
+# df_predictions = pd.DataFrame(
+#     {
+#         "SKLearn Predictions": skl_predictions,
+#         # 'SKLearn Probabilities': skl_probabilities,
+#         "ONNX Predictions": onnx_predictions,
+#         # 'ONNX Probabilities': onnx_probs
+#     }
+# )
+# print(df_predictions)
+
+
+# # Upload locally
+jaqpot = Jaqpot(
+    base_url="http://localhost.jaqpot.org",
+    app_url="http://localhost.jaqpot.org:3000",
+    login_url="http://localhost.jaqpot.org:8070",
+    api_url="http://localhost.jaqpot.org:8080",
+    keycloak_realm="jaqpot-local",
+    keycloak_client_id="jaqpot-local-test",
 )
-# print(prediction_dataset.df)
 
-skl_predictions = molecularModel_t1.predict(prediction_dataset)
-# skl_probabilities = molecularModel_t1.predict_proba(prediction_dataset)
-onnx_predictions = molecularModel_t1.predict_onnx(prediction_dataset)
-# onnx_probs = molecularModel_t1.predict_proba_onnx(prediction_dataset)
-print("SKLearn Predictions:", skl_predictions)
-# print('SKLearn Probabilities:', skl_probabilities)
-print("ONNX Predictions:", onnx_predictions)
-# print('ONNX Probabilities:', onnx_probs)
-
-
-# Merge predictions and probabilities into a pandas DataFrame
-df_predictions = pd.DataFrame(
-    {
-        "SKLearn Predictions": skl_predictions,
-        # 'SKLearn Probabilities': skl_probabilities,
-        "ONNX Predictions": onnx_predictions,
-        # 'ONNX Probabilities': onnx_probs
-    }
-)
-print(df_predictions)
-
-
-# Upload locally
-# jaqpot = Jaqpot(base_url="http://localhost.jaqpot.org", app_url="http://localhost.jaqpot.org:3000", 
-#                 login_url="http://localhost.jaqpot.org:8070",
-#                 api_url="http://localhost.jaqpot.org:8080", keycloak_realm="jaqpot-local", 
-#                 keycloak_client_id="jaqpot-local-test")
-
-jaqpot = Jaqpot()
-jaqpot.login()
-# # # jaqpot = Jaqpot("https://api.appv2.jaqpot.org")
+# # jaqpot = Jaqpot()
+# jaqpot.login()
+# # # # jaqpot = Jaqpot("https://api.appv2.jaqpot.org")
 
 # jaqpot=Jaqpot()
-# jaqpot.login()
+jaqpot.login()
 molecularModel_t1.deploy_on_jaqpot(
     jaqpot=jaqpot,
     name="Demo: Regression",
-    description="Test uploading only with onnx, without preprocessing ",
+    description="Test new api files",
     visibility="PRIVATE",
 )
