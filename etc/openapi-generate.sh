8e8779e76df1cf063e7c2cd9067826ba32f8ce53
alarv <alarvfm@gmail.com>
2024-09-30 11:28:26 +0300

    fix: update openapi generation script

--- etc/openapi-generate.sh
+++ etc/openapi-generate.sh
@@ -1,18 +1,40 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e  # Exit immediately if a command exits with a non-zero status.
 
 JAQPOT_API_PATH=../jaqpot-api
+TEMP_DIR=$(mktemp -d)
+TARGET_DIR=./jaqpotpy/api/openapi
 
-# Install openapi-python-client according to these installation instructions:
-# https://github.com/OpenAPITools/openapi-generator?tab=readme-ov-file#1---installation
+# Install openapi-generator-cli if not already installed
+if ! command -v openapi-generator &> /dev/null
+then
+    echo "openapi-generator could not be found, installing..."
+    npm install @openapitools/openapi-generator-cli -g
+fi
 
+# Update Jaqpot API
 cd $JAQPOT_API_PATH
 git pull
 cd -
 
-# Replace all DateTime occurrences with string on the openapi.yaml
+# Replace all DateTime occurrences with string in the openapi.yaml
 sed -i.bak 's/DateTime/string/g' $JAQPOT_API_PATH/src/main/resources/openapi.yaml && rm $JAQPOT_API_PATH/src/main/resources/openapi.yaml.bak
 
-# Generate the openapi client types
-openapi-generator generate -i $JAQPOT_API_PATH/src/main/resources/openapi.yaml -g python -o ./jaqpotpy/api/openapi
+# Generate the OpenAPI client in a temporary directory
+openapi-generator generate \
+    -i $JAQPOT_API_PATH/src/main/resources/openapi.yaml \
+    -g python \
+    -o $TEMP_DIR \
+    --additional-properties packageName=jaqpotpy.api.openapi
 
+# Ensure the target directory exists
+mkdir -p $TARGET_DIR
 
+# Move only the necessary files
+mv $TEMP_DIR/jaqpotpy/api/openapi/* $TARGET_DIR/
+
+# Clean up
+rm -rf $TEMP_DIR
+
+echo "OpenAPI client generated successfully in $TARGET_DIR"
