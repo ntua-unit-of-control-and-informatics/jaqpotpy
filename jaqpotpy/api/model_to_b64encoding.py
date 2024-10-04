2f6102f7a95b43bd918bb8663bab098c3af51b2a
Vassilis Minadakis <56068291+vassilismin@users.noreply.github.com>
2024-10-04 11:55:38 +0300

    Update jaqpotpy/api/model_to_b64encoding.py
    Co-authored-by: Alex Arvanitidis <alex.arvanitidis.ntua.gr@gmail.com>

--- jaqpotpy/api/model_to_b64encoding.py
+++ jaqpotpy/api/model_to_b64encoding.py
@@ -7,8 +7,8 @@ def model_to_b64encoding(actualModel):
     return model_b64encoded
 
 
-def csv_to_b64encoding(actualCsv):
-    with open(actualCsv, "rb") as file:
+def file_to_b64encoding(filePath):
+    with open(filePath, "rb") as file:
         file_data = file.read()
         b64_encoded_data = b64encode(file_data)
         return b64_encoded_data.decode("utf-8")
