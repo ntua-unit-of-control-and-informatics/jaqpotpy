db6e9c4b4cb15244ea9f195747f3efc42326a1c1
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-10-07 11:38:43 +0300

    fix: examples folder

--- examples/onnx_example.py
+++ examples/onnx_example.py
@@ -1,18 +1,15 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.feature_selection import VarianceThreshold
 
+from jaqpotpy import Jaqpot
+from jaqpotpy.datasets import JaqpotpyDataset
 from jaqpotpy.descriptors.molecular import (
-    TopologicalFingerprint,
-    MordredDescriptors,
     MACCSKeysFingerprint,
 )
-from jaqpotpy.datasets import JaqpotpyDataset
-from jaqpotpy.models import SklearnModel
 from jaqpotpy.doa.doa import Leverage
+from jaqpotpy.models import SklearnModel
 from jaqpotpy.models.preprocessing import Preprocess
-from jaqpotpy import Jaqpot
 
 path = "../jaqpotpy/test_data/test_data_smiles_classification.csv"
 
@@ -30,7 +27,6 @@ dataset = JaqpotpyDataset(
     featurizer=featurizer,
 )
 
-# sel = ["X1", "X2", "ABC"]
 sel = VarianceThreshold(threshold=0.1)
 dataset.select_features(sel)
 print(dataset.X.shape)
@@ -45,45 +41,18 @@ molecularModel_t1 = SklearnModel(
 )
 
 molecularModel_t1.fit()
-# # # print(molecularModel_t1.transformers_y)
 # pred_path = "./jaqpotpy/test_data/test_data_smiles_prediction_dataset.csv"
 # df = pd.read_csv(pred_path)
 
-# # # # df = pd.DataFrame({
-# # # #     'SMILES': ['CC'],
-# # # #     'X1': [1],
-# # # #     'X2': [2]
-# # # })
-prediction_dataset = JaqpotpyDataset(
-    df=df,
-    y_cols=None,
-    smiles_cols=smiles_cols,
-    x_cols=x_cols,
-    task="regression",
-    featurizer=featurizer,
-)
-# # # print(prediction_dataset.df)
 
-skl_predictions = molecularModel_t1.predict(prediction_dataset)
-# # skl_probabilities = molecularModel_t1.predict_proba(prediction_dataset)
+# skl_predictions = molecularModel_t1.predict(prediction_dataset)
+# skl_probabilities = molecularModel_t1.predict_proba(prediction_dataset)
 # onnx_predictions = molecularModel_t1.predict_onnx(prediction_dataset)
-# # onnx_probs = molecularModel_t1.predict_proba_onnx(prediction_dataset)
+# onnx_probs = molecularModel_t1.predict_proba_onnx(prediction_dataset)
 # print("SKLearn Predictions:", skl_predictions)
-# # print('SKLearn Probabilities:', skl_probabilities)
+# print('SKLearn Probabilities:', skl_probabilities)
 # print("ONNX Predictions:", onnx_predictions)
 # # print('ONNX Probabilities:', onnx_probs)
-
-
-# # Merge predictions and probabilities into a pandas DataFrame
-# df_predictions = pd.DataFrame(
-#     {
-#         "SKLearn Predictions": skl_predictions,
-#         # 'SKLearn Probabilities': skl_probabilities,
-#         "ONNX Predictions": onnx_predictions,
-#         # 'ONNX Probabilities': onnx_probs
-#     }
-# )
-# print(df_predictions)
 
 
 # Upload locally
