3220d8f598766aff9a3bad8a3f5ea8db7160cef6
Vassilis Minadakis <56068291+vassilismin@users.noreply.github.com>
2024-10-08 16:11:20 +0300

    feat: sdk notebook examples (#75)
    * chore: add dotenv in reqs
    
    * chore: ruff changes
    
    * chore: update python sdk examples
    
    - Uploaded a Jupyter Notebook with examples for python SDK and QsarToolBox specifically.
    
    * fix: possible values refactoring
    
    * chore: update SDK examples
    
    * fix: move the import of JaqpotApClient
    
    * Update examples/Python_SDK.ipynb
    
    Co-authored-by: Alex Arvanitidis <alex.arvanitidis.ntua.gr@gmail.com>
    
    * Update examples/Python_SDK.ipynb
    
    Co-authored-by: Alex Arvanitidis <alex.arvanitidis.ntua.gr@gmail.com>
    
    * fix: add noqa comment for linter
    
    * fix: add noqa comment for linter
    
    * fix: revert tranformation of X to numpy
    
    * chore: remove comments
    
    ---------
    
    Co-authored-by: Alex Arvanitidis <alex.arvanitidis.ntua.gr@gmail.com>

---
+++ examples/sdk_testing.py
@@ -0,0 +1,63 @@
+from jaqpotpy.jaqpot import Jaqpot
+from jaqpotpy.descriptors.molecular import TopologicalFingerprint
+from jaqpotpy.datasets import JaqpotpyDataset
+from jaqpotpy.api.openapi.models.dataset import Dataset
+from jaqpotpy.api.openapi.models import DatasetType
+
+import pandas as pd
+
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+from jaqpotpy.api.jaqpot_api_client import JaqpotApiClient  # noqa: E402
+
+jaqpot = JaqpotApiClient()
+
+# Get a model by id
+model = jaqpot.get_model_by_id(model_id=6)  # 1812)
+print(model)
+# Get model summary
+model_summary = jaqpot.get_model_summary(model_id=1837)
+print(model_summary)
+
+
+# Get shared models with organization
+shared_models = jaqpot.get_shared_models()
+print(shared_models)
+
+# Get summary of shared models with organization
+shared_models_summary = jaqpot.get_shared_models_summary()
+print(shared_models_summary)
+
+# Take a prediction with a model
+input_data = [{"SMILES": "CC", "X1": 1, "X2": 2}]
+prediction = jaqpot.predict_with_model_sync(model_id=1812, dataset=input_data)
+print(prediction)
+
+# Take prediction with a model and a csv file
+csv_path = "/Users/vassilis/Desktop/test_csv.csv"
+prediction = jaqpot.predict_with_csv(model_id=1812, csv_path=csv_path)
+print(prediction)
+
+
+# Testing QsarToolBox
+get_model = jaqpot.get_model_by_id(model_id=6)
+print(get_model)
+
+# Test QsarToolBox calculator
+prediction = jaqpot.qsartoolbox_calculator_predict_sync(
+    smiles="CC", calculator_guid="1804a854-9041-4495-9931-7414c22a5e49"
+)
+print(prediction)
+
+# Test QsarToolBox Model
+prediction = jaqpot.qsartoolbox_qsar_model_predict_sync(
+    smiles="CC", qsarGuid="c377150b-77ae-4f99-be14-357b85dd8d1f"
+)
+print(prediction)
+
+# Test QsarToolBox Profiler
+prediction = jaqpot.qsartoolbox_profiler_predict_sync(
+    smiles="CC", profilerGuid="723eb011-3e5b-4565-9358-4c3d8620ba5d"
+)
+print(prediction)
