9d3fbaf878afee1c75cb3435480aee22bc98c9c7
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-10-08 18:54:27 +0300

    chore: update sdk examples

--- examples/sdk_testing.py
+++ examples/sdk_testing.py
@@ -16,27 +16,29 @@ jaqpot = JaqpotApiClient()
 # Get a model by id
 model = jaqpot.get_model_by_id(model_id=6)  # 1812)
 print(model)
-# Get model summary
-model_summary = jaqpot.get_model_summary(model_id=1837)
-print(model_summary)
 
+# Get model summary
+model_summary = jaqpot.get_model_summary(model_id=1812)
+print(model_summary)
 
 # Get shared models with organization
 shared_models = jaqpot.get_shared_models()
 print(shared_models)
 
-# Get summary of shared models with organization
-shared_models_summary = jaqpot.get_shared_models_summary()
-print(shared_models_summary)
-
-# Take a prediction with a model
+# Take a synchronous prediction with a model
 input_data = [{"SMILES": "CC", "X1": 1, "X2": 2}]
-prediction = jaqpot.predict_with_model_sync(model_id=1812, dataset=input_data)
+prediction = jaqpot.predict_sync(model_id=1812, dataset=input_data)
 print(prediction)
+
+# Take an asynchronous prediction with a model
+input_data = [{"SMILES": "CC", "X1": 1, "X2": 2}]
+prediction = jaqpot.predict_async(model_id=1812, dataset=input_data)
+prediction_dataset = jaqpot.get_dataset(response=prediction)
+print(prediction_dataset)
 
 # Take prediction with a model and a csv file
 csv_path = "/Users/vassilis/Desktop/test_csv.csv"
-prediction = jaqpot.predict_with_csv(model_id=1812, csv_path=csv_path)
+prediction = jaqpot.predict_with_csv_sync(model_id=1812, csv_path=csv_path)
 print(prediction)
 
 
@@ -52,12 +54,12 @@ print(prediction)
 
 # Test QsarToolBox Model
 prediction = jaqpot.qsartoolbox_qsar_model_predict_sync(
-    smiles="CC", qsarGuid="c377150b-77ae-4f99-be14-357b85dd8d1f"
+    smiles="CC", qsar_guid="c377150b-77ae-4f99-be14-357b85dd8d1f"
 )
 print(prediction)
 
 # Test QsarToolBox Profiler
 prediction = jaqpot.qsartoolbox_profiler_predict_sync(
-    smiles="CC", profilerGuid="723eb011-3e5b-4565-9358-4c3d8620ba5d"
+    smiles="CC", profiler_guid="723eb011-3e5b-4565-9358-4c3d8620ba5d"
 )
 print(prediction)
