e73e3e5cc4e10eae771fc0dace01bb336db42d6c
Periklis_ts <ptsirostsib@gmail.com>
2024-10-10 16:39:20 +0300

    chore: examples

--- examples/preprocessing_example.py
+++ examples/preprocessing_example.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import sklearn
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import (
+    StandardScaler,
+    MinMaxScaler,
+    LabelEncoder,
+    OneHotEncoder,
+)
 from sklearn.feature_selection import VarianceThreshold
+from sklearn.compose import ColumnTransformer
 
 from jaqpotpy import Jaqpot
 from jaqpotpy.datasets import JaqpotpyDataset
@@ -31,14 +37,15 @@ dataset = JaqpotpyDataset(
 
 
 model = RandomForestRegressor(random_state=42)
-molecularModel_t1 = SklearnModel(dataset=dataset, model=model, preprocess_x=None)
-molecularModel_t1 = SklearnModel(
-    dataset=dataset, model=model, preprocess_x=VarianceThreshold()
-)
-molecularModel_t1 = SklearnModel(
-    dataset=dataset, model=model, preprocess_y=VarianceThreshold()
-)
 
 molecularModel_t1 = SklearnModel(
-    dataset=dataset, model=model, preprocess_x=StandardScaler()
+    dataset=dataset,
+    model=model,
+    preprocess_x=ColumnTransformer(
+        transformers=[
+            # ("Standard Scaler", StandardScaler(), ["X1", "X2"]),
+            ("OneHotEncoder", OneHotEncoder(), ["Cat_col"]),
+        ],
+        remainder="passthrough",
+    ),
 )
