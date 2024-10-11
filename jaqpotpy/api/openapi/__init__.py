464aef08865a14a531e38ee5ccb91f5ade89ace9
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-10-11 13:47:53 +0300

    chore: update openapi files

--- jaqpotpy/api/openapi/__init__.py
+++ jaqpotpy/api/openapi/__init__.py
@@ -40,11 +40,15 @@ from jaqpotpy.api.openapi.exceptions import ApiException
 
 # import models into sdk package
 from jaqpotpy.api.openapi.models.api_key import ApiKey
+from jaqpotpy.api.openapi.models.bounding_box_doa import BoundingBoxDoa
+from jaqpotpy.api.openapi.models.city_block_doa import CityBlockDoa
 from jaqpotpy.api.openapi.models.create_api_key201_response import CreateApiKey201Response
 from jaqpotpy.api.openapi.models.create_invitations_request import CreateInvitationsRequest
 from jaqpotpy.api.openapi.models.dataset import Dataset
 from jaqpotpy.api.openapi.models.dataset_csv import DatasetCSV
 from jaqpotpy.api.openapi.models.dataset_type import DatasetType
+from jaqpotpy.api.openapi.models.doa import Doa
+from jaqpotpy.api.openapi.models.doa_data import DoaData
 from jaqpotpy.api.openapi.models.error_code import ErrorCode
 from jaqpotpy.api.openapi.models.error_response import ErrorResponse
 from jaqpotpy.api.openapi.models.feature import Feature
@@ -53,8 +57,12 @@ from jaqpotpy.api.openapi.models.feature_type import FeatureType
 from jaqpotpy.api.openapi.models.get_all_api_keys_for_user200_response_inner import GetAllApiKeysForUser200ResponseInner
 from jaqpotpy.api.openapi.models.get_datasets200_response import GetDatasets200Response
 from jaqpotpy.api.openapi.models.get_models200_response import GetModels200Response
+from jaqpotpy.api.openapi.models.kernel_based_doa import KernelBasedDoa
 from jaqpotpy.api.openapi.models.lead import Lead
+from jaqpotpy.api.openapi.models.leverage_doa import LeverageDoa
 from jaqpotpy.api.openapi.models.library import Library
+from jaqpotpy.api.openapi.models.mahalanobis_doa import MahalanobisDoa
+from jaqpotpy.api.openapi.models.mean_var_doa import MeanVarDoa
 from jaqpotpy.api.openapi.models.model import Model
 from jaqpotpy.api.openapi.models.model_extra_config import ModelExtraConfig
 from jaqpotpy.api.openapi.models.model_summary import ModelSummary
