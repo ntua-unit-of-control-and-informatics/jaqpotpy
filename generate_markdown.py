4308906bf1057e0cf4441c87e4c45e31d5e4eef4
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-11-04 17:03:15 +0200

    feat: generate markdown folder pydoc_docs

--- generate_markdown.py
+++ generate_markdown.py
@@ -1,11 +1,12 @@
 from pathlib import Path
-from pydoc_markdown.interfaces import Context
+from pydoc_markdown import PydocMarkdown
 from pydoc_markdown.contrib.loaders.python import PythonLoader
 from pydoc_markdown.contrib.renderers.markdown import MarkdownRenderer
 
+session = PydocMarkdown()
+
 # Set the root folder
-root_folder = Path("./")
-context = Context(directory=root_folder)
+root_folder = Path(".")
 
 # Initialize the Markdown renderer
 renderer = MarkdownRenderer(render_module_header=False)
@@ -22,16 +23,13 @@ dirs_rglob = [
 for folder in dirs_rglob:
     print(f"Processing folder: {folder}")
 
-    # Initialize the Python loader and renderer
-    loader = PythonLoader(search_path=[folder])
-    loader.init(context)
-    renderer.init(context)
+    session.loaders[0].search_path = [folder]
 
-    # Load and render the modules
-    modules = loader.load()
-    markdown = renderer.render_to_string(modules)
-    # print(f"Rendered Markdown:\n{markdown}")
+    modules = session.load_modules()
+    session.process(modules)
+    markdown = session.renderer.render_to_string(modules)
 
+    print(markdown)
     # Save the Markdown to a file
     output_file = Path(f"pydoc_docs/{folder.name}.md")
     output_file.parent.mkdir(exist_ok=True, parents=True)
