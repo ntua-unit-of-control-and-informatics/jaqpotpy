4f693c3fec1931de9d7dc18033b8b1817759cfda
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-11-04 16:34:43 +0200

    feat(JAQPOT-330): generate markdown from pydoc

---
+++ generate_markdown.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from pydoc_markdown.interfaces import Context
+from pydoc_markdown.contrib.loaders.python import PythonLoader
+from pydoc_markdown.contrib.renderers.markdown import MarkdownRenderer
+
+# Set the root folder
+root_folder = Path("./")
+context = Context(directory=root_folder)
+
+# Initialize the Markdown renderer
+renderer = MarkdownRenderer(render_module_header=False)
+
+ignored_folders_prefix = ("_", "__", ".", "test", "colorlog", "cfg", "api")
+# Find all relevant directories in the 'jaqpotpy' folder
+dirs_rglob = [
+    x
+    for x in root_folder.glob("jaqpotpy/*/")
+    if x.is_dir() and not any(x.name.startswith(p) for p in ignored_folders_prefix)
+]
+
+# Process each directory
+for folder in dirs_rglob:
+    print(f"Processing folder: {folder}")
+
+    # Initialize the Python loader and renderer
+    loader = PythonLoader(search_path=[folder])
+    loader.init(context)
+    renderer.init(context)
+
+    # Load and render the modules
+    modules = loader.load()
+    markdown = renderer.render_to_string(modules)
+    # print(f"Rendered Markdown:\n{markdown}")
+
+    # Save the Markdown to a file
+    output_file = Path(f"pydoc_docs/{folder.name}.md")
+    output_file.parent.mkdir(exist_ok=True, parents=True)
+    output_file.write_text(markdown)
