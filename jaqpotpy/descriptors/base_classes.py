bbcb9d028b8684600c818e63e89dad36ec6af564
Periklis_ts <ptsirostsib@gmail.com>
2024-11-05 11:26:15 +0200

    chore: deleted config from base_classes

--- jaqpotpy/descriptors/base_classes.py
+++ jaqpotpy/descriptors/base_classes.py
@@ -4,7 +4,7 @@ import logging
 from typing import Any, List, Iterable
 import pandas as pd
 from tqdm import tqdm
-from jaqpotpy.cfg import config
+
 
 logger = logging.getLogger(__name__)
 _print_threshold = 1000
