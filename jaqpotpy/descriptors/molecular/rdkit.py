728332bb1a7c471d5386cf36c8172e3a187d993f
john savvas <149728671+johnsaveus@users.noreply.github.com>
2024-11-08 09:52:05 +0200

    fix: minor_fix_rdkit (#115)
    * minor_fix
    
    * unit_tests
    
    * new_xgboost_ready
    
    * Supporting early_stopping
    
    * .

--- jaqpotpy/descriptors/molecular/rdkit.py
+++ jaqpotpy/descriptors/molecular/rdkit.py
@@ -35,10 +35,6 @@ class RDKitDescriptors(MolecularFeaturizer):
         """
         self.use_fragment = use_fragment
         self.ipc_avg = ipc_avg
-        self.descriptors = []
-        self.descList = []
-        self.col_names = []
-        self.counter = 0
 
     def _featurize(self, datapoint, **kwargs) -> np.ndarray:
         """Calculate RDKit descriptors.
@@ -47,31 +43,31 @@ class RDKitDescriptors(MolecularFeaturizer):
         Returns: features --> np.ndarray
         -------
         """
-        # initialize
-        if len(self.descList) == 0:
-            try:
-                for descriptor, function in Descriptors.descList:
-                    if self.use_fragment is False and descriptor.startswith("fr_"):
-                        continue
-                    self.descriptors.append(descriptor)
-                    self.descList.append((descriptor, function))
-            except ModuleNotFoundError:
-                raise ImportError("This class requires RDKit to be installed.")
-
-        # check initialization
-        assert len(self.descriptors) == len(self.descList)
-
+        descList = self.get_desc_list()
         features = []
-        for desc_name, function in self.descList:
+        for desc_name, function in descList:
             if desc_name == "Ipc" and self.ipc_avg:
                 feature = function(datapoint, avg=True)
             else:
                 feature = function(datapoint)
             features.append(feature)
-            if self.counter == 0:
-                self.col_names.append(desc_name)
-        self.counter += 1
         return np.asarray(features)
+
+    def get_desc_list(self):
+        descList = []
+        try:
+            for descriptor, function in Descriptors.descList:
+                if self.use_fragment is False and descriptor.startswith("fr_"):
+                    continue
+                descList.append((descriptor, function))
+        except ModuleNotFoundError:
+            raise ImportError("This class requires RDKit to be installed.")
+        return descList
+
+    def get_desc_names(self):
+        descList = self.get_desc_list()
+        col_names = [desc_name for desc_name, _ in descList]
+        return col_names
 
     def featurize_dataframe(
         self, datapoints, convert_nan: bool = False, log_every_n=1000, **kwargs
@@ -83,5 +79,5 @@ class RDKitDescriptors(MolecularFeaturizer):
             - If ignore_3D is False, the length is 1826.
         """
         features = self.featurize(datapoints, convert_nan, log_every_n, **kwargs)
-        df = pd.DataFrame(features, columns=self.col_names)
+        df = pd.DataFrame(features, columns=self.get_desc_names())
         return df
