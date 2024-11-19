991e8f11fd09b5a6cba1e75c2d5a2e18c9305037
alarv <alarvfm@gmail.com>
2024-11-19 12:04:02 +0200

    chore: rename to jaqpotpy on setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="src",
+    name="jaqpotpy",
     version="{{VERSION_PLACEHOLDER}}",
     description="Client library for managing machine learning models on the Jaqpot platform",
     long_description=long_description,
