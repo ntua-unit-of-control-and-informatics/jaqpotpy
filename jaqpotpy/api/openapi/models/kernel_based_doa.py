ba405191579754371eac12fefcf3bd0f64600d44
periklis91 <periklis91@users.noreply.github.com>
2024-12-03 13:46:44 +0000

    chore: update openapi files

--- jaqpotpy/api/openapi/models/kernel_based_doa.py
+++ jaqpotpy/api/openapi/models/kernel_based_doa.py
@@ -31,10 +31,8 @@ class KernelBasedDoa(BaseModel):
     gamma: Optional[Union[StrictFloat, StrictInt]] = None
     threshold: Optional[Union[StrictFloat, StrictInt]] = None
     kernel_type: Optional[StrictStr] = Field(default=None, alias="kernelType")
-    threshold_method: Optional[StrictStr] = Field(default=None, alias="thresholdMethod")
-    threshold_percentile: Optional[Union[StrictFloat, StrictInt]] = Field(default=None, alias="thresholdPercentile")
     data_points: Optional[List[List[Union[StrictFloat, StrictInt]]]] = Field(default=None, alias="dataPoints")
-    __properties: ClassVar[List[str]] = ["sigma", "gamma", "threshold", "kernelType", "thresholdMethod", "thresholdPercentile", "dataPoints"]
+    __properties: ClassVar[List[str]] = ["sigma", "gamma", "threshold", "kernelType", "dataPoints"]
 
     @field_validator('kernel_type')
     def kernel_type_validate_enum(cls, value):
@@ -101,8 +99,6 @@ class KernelBasedDoa(BaseModel):
             "gamma": obj.get("gamma"),
             "threshold": obj.get("threshold"),
             "kernelType": obj.get("kernelType"),
-            "thresholdMethod": obj.get("thresholdMethod"),
-            "thresholdPercentile": obj.get("thresholdPercentile"),
             "dataPoints": obj.get("dataPoints")
         })
         return _obj
