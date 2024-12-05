19ca9351e6a410fb760dc8efa6d4df3e6a8f5475
john savvas <ioannis.savvas00@gmail.com>
2024-12-05 11:41:31 +0200

    float128_bug

--- jaqpotpy/descriptors/molecular/mordred.py
+++ jaqpotpy/descriptors/molecular/mordred.py
@@ -2,7 +2,8 @@ from typing import Callable, List, Optional, Any
 import numpy as np
 import pandas as pd
 from jaqpotpy.descriptors.base_classes import MolecularFeaturizer
-# warnings.filterwarnings("ignore", category=DeprecationWarning)
+from mordred import Calculator, descriptors, is_missing
+from rdkit import Chem
 
 
 class MordredDescriptors(MolecularFeaturizer):
@@ -39,10 +40,7 @@ class MordredDescriptors(MolecularFeaturizer):
         ignore_3D: bool, optional (default True)
             Whether to use 3D information or not.
         """
-        self.col_names = None
         self.ignore_3D = ignore_3D
-        self.calc: Optional[Callable] = None
-        self.descriptors: Optional[List] = None
 
     def _featurize(self, datapoint, convert_nan: bool = True, **kwargs) -> np.ndarray:
         """Calculate Mordred descriptors for a single molecule.
@@ -61,25 +59,22 @@ class MordredDescriptors(MolecularFeaturizer):
             If ignore_3D is True, the length is 1613.
             If ignore_3D is False, the length is 1826.
         """
-        if self.calc is None:
-            try:
-                from mordred import Calculator, descriptors, is_missing
 
-                self.is_missing = is_missing
-                self.calc = Calculator(descriptors, ignore_3D=self.ignore_3D)
-                self.descriptors = list(descriptors.__all__)
-            except ModuleNotFoundError:
-                raise ImportError("This class requires Mordred to be installed.")
-
-        feature = self.calc(datapoint)
-        self.col_names = [key for key in feature.keys()]
+        calc = Calculator(descriptors, ignore_3D=self.ignore_3D)
+        feature = calc(datapoint)
         # convert errors to zero
         if convert_nan:
             feature = [
-                -1000.0 if self.is_missing(val) or isinstance(val, str) else val
+                -1000.0 if is_missing(val) or isinstance(val, str) else val
                 for val in feature
             ]
-        return np.asarray(feature)
+        return np.asarray(feature, dtype=np.float64)
+
+    def get_desc_names(self):
+        calc = Calculator(descriptors, ignore_3D=self.ignore_3D)
+        datapoint = Chem.MolFromSmiles("CCC")
+        col_names = [key for key in calc(datapoint).keys()]
+        return col_names
 
     def featurize_dataframe(
         self, datapoints, convert_nan: bool = True, log_every_n=1000, **kwargs
@@ -103,5 +98,5 @@ class MordredDescriptors(MolecularFeaturizer):
             If ignore_3D is False, the length is 1826.
         """
         features = self.featurize(datapoints, convert_nan, log_every_n, **kwargs)
-        df = pd.DataFrame(features, columns=self.col_names)
+        df = pd.DataFrame(features, columns=self.get_desc_names())
         return df
