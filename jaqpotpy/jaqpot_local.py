cd59259d758f36c0b8e7c18a6632c72e52692982
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2025-04-30 14:26:34 +0300

    feat: add JaqpotLocal

---
+++ jaqpotpy/jaqpot_local.py
@@ -0,0 +1,13 @@
+from jaqpotpy import Jaqpot
+
+
+class JaqpotLocal(Jaqpot):
+    def __init__(self):
+        super().__init__(
+            base_url="http://localhost.jaqpot.org",
+            app_url="http://localhost.jaqpot.org:3000",
+            login_url="http://localhost.jaqpot.org:8070",
+            api_url="http://localhost.jaqpot.org:8080",
+            keycloak_realm="jaqpot-local",
+            keycloak_client_id="jaqpot-local-test",
+        )
