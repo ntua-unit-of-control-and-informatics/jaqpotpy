9694e980447b79bb39630be69a142f85df94ef17
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2025-05-14 10:27:28 +0300

    fix: revert changes to files

--- examples/sklearn/upload_a_model.py
+++ examples/sklearn/upload_a_model.py
@@ -4,22 +4,14 @@ from sklearn.ensemble import RandomForestClassifier
 
 from jaqpotpy.datasets import JaqpotTabularDataset
 from sklearn.linear_model import LogisticRegression
-
-from jaqpotpy.jaqpot_local import JaqpotLocal
 from jaqpotpy.models import SklearnModel
 
 # Generate a small binary classification dataset
-X, y = make_classification(
-    n_samples=100,
-    n_features=5,
-    n_informative=3,
-    n_classes=3,  # This will generate labels 0-4
-    random_state=42,
-)
+X, y = make_classification(n_samples=100, n_features=4, random_state=42)
 
 # Create a DataFrame with the features and target
 # Store the features in columns "X1", "X2", "X3", "X4" and the target in column "y".
-df = pd.DataFrame(X, columns=["X1", "X2", "X3", "X4", "X5"])
+df = pd.DataFrame(X, columns=["X1", "X2", "X3", "X4"])
 df["y"] = y
 
 # Initialize a JaqpotpyDataset with the DataFrame
@@ -28,12 +20,12 @@ dataset = JaqpotTabularDataset(
     df=df,
     x_cols=["X1", "X2", "X3", "X4"],
     y_cols=["y"],
-    task="multiclass_classification",
+    task="binary_classification",
 )
 
 # Wrap the scikit-learn model with Jaqpotpy's SklearnModel
 # Use Logistic Regression as the classification model.
-jaqpot_model = SklearnModel(dataset=dataset, model=RandomForestClassifier())
+jaqpot_model = SklearnModel(dataset=dataset, model=LogisticRegression())
 
 # Fit the model to the dataset
 # Train the Logistic Regression model using the provided dataset.
@@ -44,7 +36,7 @@ jaqpot_model.fit()
 from jaqpotpy import Jaqpot  # noqa: E402
 
 # Next, create an instance of Jaqpot
-jaqpot = JaqpotLocal()
+jaqpot = Jaqpot()
 # Then login to Jaqpot. jaqpot.login will prompt you to enter
 # an authorization code that you will receive from your
 #  browser after you login to Jaqpot.
@@ -55,7 +47,7 @@ jaqpot.login()
 # will be able to see it and use it to take predictions.
 jaqpot_model.deploy_on_jaqpot(
     jaqpot=jaqpot,
-    name="Multiclass classification",
+    name="My first Jaqpot Model",
     description="This is my first attempt to train and upload a Jaqpot model.",
     visibility="PRIVATE",
 )
